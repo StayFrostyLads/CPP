bc697f214df600cbbc5c5c6466c3bb88cdb66c65
StayFrostyLads <59467621+StayFrostyLads@users.noreply.github.com>
2022-05-24 09:11:59 -0600

    Create AmongUsPanel.java

---
+++ src/FourierSeries/AmongUsPanel.java
@@ -0,0 +1,34 @@
+package panel;
+
+import javax.swing.*;
+
+/*
+ * Class for creating GUI specifics for one of the image JPanels.
+ * 
+ * @author Jack Nebeker
+ * @version 1.0.0
+ * @since 2022-03-23
+ * 
+ */
+public class AmongUsPanel extends JPanel {
+	
+	public static JButton home, quit;
+	private JLabel title;
+	
+	public AmongUsPanel() {
+		setLayout(null);
+		title = new JLabel("Among Us Character"); 
+		title.setBounds(325,20,200,100);
+		add(title);
+		
+		home = new JButton("Back to Home");
+		home.setBounds(250,600,300,50);
+		add(home);
+		
+		quit = new JButton("Exit Application");
+		quit.setBounds(620,20,150,50);
+		add(quit);
+		
+	}
+
+}
